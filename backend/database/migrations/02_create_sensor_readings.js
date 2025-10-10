25223a74a16867bc055399142714d9985f45789f
GilangFauzi7 <gilangfauzi72@gmail.com>
2025-10-10 14:13:11 +0700

    fix: delete table column

--- backend/database/migrations/02_create_sensor_readings.js
+++ backend/database/migrations/02_create_sensor_readings.js
@@ -5,16 +5,29 @@
 exports.up = function (knex) {
 	return knex.schema.createTable("sensor_readings", function (table) {
 		table.increments("id").primary();
-		table.timestamp("insert_timestamp", { useTz: false }).defaultTo(knex.fn.now());
+		table
+			.timestamp("insert_timestamp", { useTz: false })
+			.defaultTo(knex.fn.now());
 		table
 			.string("mac_address")
 			.references("mac_address")
 			.inTable("devices")
 			.onDelete("CASCADE");
-		table.integer("red_information").notNullable().defaultTo(0).checkIn([0, 1]);
-		table.integer("amber_information").notNullable().defaultTo(0).checkIn([0, 1]);
-		table.integer("green_information").notNullable().defaultTo(0).checkIn([0, 1]);
-		table.string("working_shift");
+		table
+			.integer("red_information")
+			.notNullable()
+			.defaultTo(0)
+			.checkIn([0, 1]);
+		table
+			.integer("amber_information")
+			.notNullable()
+			.defaultTo(0)
+			.checkIn([0, 1]);
+		table
+			.integer("green_information")
+			.notNullable()
+			.defaultTo(0)
+			.checkIn([0, 1]);
 	});
 };
 
